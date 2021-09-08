e29648c91443d660ef94b60d21b821b543032b8f
Tim <tim.geimer@capgemini.com>
2021-09-08 18:57:37 +0200

    cleanups

--- deployments/k8s-weu.tf
+++ deployments/k8s-weu.tf
@@ -11,7 +11,6 @@ module "instance-weu" {
     
     prefix   = "tim"
     location = "West Europe"
-    #kubernetes_service_subnets = ["172.16.0.0/16"]
     networks = {
         external = "10.0.0.0/24"
         internal = "172.16.0.0/16"
@@ -24,10 +23,8 @@ module "instance-weu" {
 module "instance-1-np-1" {
     source = "../modules/iaas/kubernetesnodepool"
 
-    name = "tk8si1np1" #max 8 chars
+    name = "np1"
     #max_count = 1
     #vm_size = "Standard_D1_v2"
     kubernetes_cluster = module.instance-weu.kubernetes_cluster
-    #labels = {}
-    #taints = []
 }
