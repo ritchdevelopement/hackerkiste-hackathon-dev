6ff73e3c523c949f60a1ef77e9b15181aeaf068d
Tim <tim.geimer@capgemini.com>
2021-09-13 16:43:15 +0200

    Create a lightweight version

--- k8s-weu.tf
+++ k8s-weu.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name  = "meta"
     storage_account_name = "hackathonterraform"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "light.terraform.tfstate"
   }
 }
 
@@ -18,24 +18,56 @@ provider "azurerm" {
   features {}
 }
 
-module "cluster-weu" {
-  source = "./modules/k8scluster"
-
-  prefix   = "tim"
-  location = "West Europe"
-  node_network = "10.0.0.0/24"
-  kubernetes_version = "1.21.2"
+variable "name" {
+  type        = string
+  description = "Name den die Resourcen in Azure haben sollen. Muss alphanumerisch sein."
+  default     = "light"
 }
 
-module "cluster-weu-np1" {
-  source = "./modules/k8snodepool"
+resource "azurerm_resource_group" "global" {
+  name     = var.name
+  location = "West Europe"
+}
 
-  name = "np1"
-  #max_count = 1
-  #vm_size = "Standard_D1_v2"
-  kubernetes_cluster = module.cluster-weu.kubernetes_cluster
+# AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.name
+  resource_group_name = azurerm_resource_group.global.name
+  location            = azurerm_resource_group.global.location
+  dns_prefix          = var.name
+  node_resource_group = format("%s-%s", azurerm_resource_group.global.name, "aksrg")
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Referenz zu einer extern erstellten Container Registry
+data "azurerm_container_registry" "acr" {
+  name                = "2021hackathon"
+  resource_group_name = "meta"
+}
+
+# Registry Pull Berechtigung für das Cluster
+resource "azurerm_role_assignment" "acr" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
 }
 
 output "info" {
-  value = format("Um auf das Cluster zuzugreifen öffne die Azure Shell und führe die folgenden Befehle aus:\n\naz aks get-credentials --resource-group %s --name %s\nkubectl get nodes\n", module.cluster-weu.kubernetes_cluster.resource_group_name, module.cluster-weu.kubernetes_cluster.name)
+  value = format(<<EOT
+    Um auf das Cluster zuzugreifen öffne die Azure Shell und führe die folgenden Befehle aus:
+    
+    az aks get-credentials --resource-group %s --name %s
+    kubectl get nodes
+    EOT
+    , azurerm_kubernetes_cluster.aks.resource_group_name,
+    azurerm_kubernetes_cluster.aks.name)
 }
