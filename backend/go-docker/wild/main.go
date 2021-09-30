1b7369f82caf9857bcb5183d664a62e0c07b4779
Andris <andris@MacBook-Air-von-Andris.local>
2021-09-30 19:02:25 +0200

    added language type and server ip to response

--- backend/go-docker/wild/main.go
+++ backend/go-docker/wild/main.go
@@ -1,10 +1,38 @@
 package wild
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net"
 	"net/http"
 )
 
+type response struct {
+	LangType string `json:"language_type"`
+	Ip       string `json:"server_ip"`
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("Index Resource")))
+	responseJson := response{
+		LangType: "golang",
+		Ip:       GetOutboundIP(),
+	}
+
+	enc := json.NewEncoder(w)
+	enc.Encode(responseJson)
+
+	//w.Write([]byte(fmt.Sprintf("Index Resource ", responseJson)))
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String()
 }
